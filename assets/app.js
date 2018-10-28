12bb6f42e25b2e1de678893df20c3e22f558210e
C Howe <brook.theis@gmail.com>
2018-10-27 22:32:47 -0600

    Added gifs to the page, created still default and on-click animation

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,82 @@
 //DEFINE global variables
     //CREATE ARRAY of strings
 var topics = ["cat","dog","hamster","fish","bird","sheep","monkey"]
-    //for loop-- take string name, make into button, give it a data value
+    
 for (i = 0; i < topics.length; i++) {
     //CREATE new button
     var button = $("<button>");
+    //STORE string in new variable
+    var animalName = topics[i];
     //SET text equal to string in array
-    button.text(topics[i]);
+    button.text(animalName);
+    //GIVE each button data attribute equal to string
+    button.attr("data-animal", animalName);
     //APPEND buttons to animals div
     $("#animals").append(button);
 }
 
 //ON CLICK
+$("button").on("click", function() {
+ // GET data attribute from click
+ var animal = $(this).attr("data-animal");
+ //API url, store as variable
+ //edit query url to call gif related to string, limit 10 gifs
+ var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal +
+ "&api_key=MUZEBvJnYr2rv9aIpEMkS5AMc9UYchXq&limit=10" ;
  //ajax call
- //edit ajax url to call gif related to string
- //limit 10 gifs
- //gifs paused by default, animate on click
- //display rating above
+ $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+        console.log(response);
+
+        var results = response.data;
+
+       //for loop to display gifs
+       for (var i = 0; i < results.length; i ++) {
+         //create new div to hold each gif
+            var gifDiv = $("<div>");
+         //save rating in variable
+            var rating = results[i].rating;
+         //create paragraph
+            var p = $("<p>");
+         //Display rating in the p tag
+            p.text(rating);
+         //create new image
+            var animalImage = $("<img>");
+         //Give new image src from the results
+            animalImage.attr("src", results[i].images.fixed_height_still.url);
+         //give a data-still attr
+            animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+         //give a data-animate attr
+            animalImage.attr("data-animate", results[i].images.fixed_height.url);
+         //Set data-state to still
+            animalImage.attr("data-state", "still");
+         //Append rating to gifDiv
+            gifDiv.append(p);
+         //Append gif image to gifDiv
+            gifDiv.append(animalImage);
+         //display gifs on page
+            $("#gifs").prepend(gifDiv);
+        }
+
+        
+        //On click, animate gifs
+        $("img").on("click", function() {
+            //create variable referring to the state of image clicked
+            var state = $(this).attr("data-state");
+            //if image is still, change img src to animated
+            if (state === "still"){
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state","animate");
+            //if image is animated, change img src to still
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        })
+    })
+})
 
 //FORM
     //when user clicks submit, save string to the array
